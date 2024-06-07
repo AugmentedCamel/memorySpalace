16084319e737b363ee2e20c95943e24f7c1a60fb
lauramurinova <lauramurinova@gmail.com>
2024-06-07 16:37:03 -0300

    Updated speech scripts

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -4,110 +4,245 @@ using UnityEngine.Events;
 
 public class SpeechBubble : MonoBehaviour
 {
-    [SerializeField] private MenuBubbleInteractor _bubbleInteractor;
-    [SerializeField] private TextMeshPro _text;
+    #region SerializeFiled variables - need references
+    
+    // the bubbles recorded text
+    [SerializeField] private TextMeshPro _recordedText;
+    
+    // percentage text
     [SerializeField] private TextMeshPro _percentageSimilarity;
+    
+    // audio source to play the clip
     [SerializeField] private AudioSource _audioSource;
+    
+    #endregion
 
+    #region Private Variables
+    
     private UnityEvent<string> _speechTextEvent = new UnityEvent<string>();
     private UnityEvent<string> _partialTranscriptionEvent = new UnityEvent<string>();
     private UnityEvent<AudioClip> _speechAudioEvent = new UnityEvent<AudioClip>();
     private string _currentString = "";
     private AudioClip _audioClip = null;
     
-    private void Start()
-    {
-        _bubbleInteractor.onTriggerEnteredEvent.AddListener(delegate
-        {
-            RecordClipAndText();
-            _bubbleInteractor.onTriggerEnteredEvent.RemoveAllListeners();
-            
-            // second try should write down the percentage
-            _bubbleInteractor.onTriggerEnteredEvent.AddListener(delegate
-            {
-                TestUser();
-                Invoke(nameof(RepeatAudioClip), 3f);
-            });
-        });
-    }
-    
-    private void TestUser()
-    {
-        SpeechToTextManager.Instance.StartRecording(_speechTextEvent);
-        // set recognized text
-        _speechTextEvent.RemoveAllListeners();
-        _speechTextEvent.AddListener(recognizedText =>
-        {
-            _percentageSimilarity.text = recognizedText + " percentage: " + SpeechToTextManager.Instance.CalculateSimilarityPercentage(_currentString, recognizedText).ToString();
-            _speechTextEvent.RemoveAllListeners();
-        });
-    }
+    #endregion
 
+    #region Private Record Methods
+    
+    /// <summary>
+    /// Records partial text, full text and audio of users input.
+    /// </summary>
     private void RecordClipAndText()
     {
+        _recordedText.text = "";
+        _percentageSimilarity.text = "";
+        _audioClip = null;
+        
         SpeechToTextManager.Instance.StartRecording(_speechTextEvent, _partialTranscriptionEvent, _speechAudioEvent);
-        // set recognized text
         
-        _partialTranscriptionEvent.RemoveAllListeners();
-        _partialTranscriptionEvent.AddListener(partialText =>
-        {
-            _text.text = partialText;
-        });
+        // listen to partial recognized text
+        RecordPartialText();
         
-        _speechTextEvent.RemoveAllListeners();
-        _speechTextEvent.AddListener(recognizedText =>
-        {
-            _currentString = recognizedText;
-            _text.text = recognizedText;
-            _speechTextEvent.RemoveAllListeners();
-            _partialTranscriptionEvent.RemoveAllListeners();
-        });
-                
-        // set recorded audioclip
+        // record the full text
+        RecordFullText();
+
+        // listen to audio recording
+        RecordAudio();
+    }
+
+    /// <summary>
+    /// Initiates audio recording.
+    /// </summary>
+    private void RecordAudio()
+    {
         _speechAudioEvent.AddListener(recordedAudio =>
         {
             _audioClip = recordedAudio;
-            _speechAudioEvent.RemoveAllListeners();
         });
     }
 
-    public void Rerecord()
+    /// <summary>
+    /// Initiates full transcription of users input.
+    /// </summary>
+    private void RecordFullText()
+    {
+        _speechTextEvent.RemoveAllListeners();
+        _speechTextEvent.AddListener(recognizedText =>
+        {
+            _currentString += " " + recognizedText;
+            _recordedText.text = _currentString;
+        });
+    }
+
+    /// <summary>
+    /// Initiates partial transcription of users input.
+    /// </summary>
+    private void RecordPartialText()
+    {
+        _partialTranscriptionEvent.RemoveAllListeners();
+        _partialTranscriptionEvent.AddListener(partialText =>
+        {
+            _recordedText.text = _currentString + " " + partialText;
+        });
+    }
+
+    /// <summary>
+    /// Stops all listeners form recording audio and text.
+    /// </summary>
+    private void StopRecordings()
+    {
+        SpeechToTextManager.Instance.StopRecording();
+        
+        _speechTextEvent.RemoveAllListeners();
+        _partialTranscriptionEvent.RemoveAllListeners();
+        _speechAudioEvent.RemoveAllListeners();
+    }
+    
+    /// <summary>
+    /// Initiates comparison of the bubble data and users input.
+    /// </summary>
+    private void TestUser()
+    {
+        SpeechToTextManager.Instance.StartRecording(_speechTextEvent);
+        
+        // listen to recognized text
+        _speechTextEvent.RemoveAllListeners();
+        _speechTextEvent.AddListener(recognizedText =>
+        {
+            _percentageSimilarity.text = " Success Rate: " + SpeechToTextManager.Instance.CalculateSimilarityPercentage(_currentString, recognizedText).ToString("F");
+            _speechTextEvent.RemoveAllListeners();
+        });
+    }
+    
+    #endregion
+
+    #region Public Methods for the UI
+    
+    /// <summary>
+    /// Method to call the comparison between bubbles set text and users input.
+    /// </summary>
+    public void Test()
+    {
+        TestUser();
+    }
+
+    /// <summary>
+    /// Method to record AudioClip and text of what the user is saying.
+    /// </summary>
+    public void StartRecording()
     {
         RecordClipAndText();
     }
 
+    /// <summary>
+    /// Method to stop recording audio and text.
+    /// </summary>
+    public void StopRecording()
+    {
+        StopRecordings();
+    }
+
+    /// <summary>
+    /// Toggles active/inactive the text of the bubble.
+    /// </summary>
+    public void ToggleTranscribedText()
+    {
+        if (_recordedText.gameObject.activeSelf)
+        {
+            HideTranscribedText();
+        }
+        else
+        {
+            ShowTranscribedText();
+        }
+    }
+
+    /// <summary>
+    /// Shows the text of the bubble.
+    /// </summary>
     public void ShowTranscribedText()
     {
-        _text.text = _currentString;
-        _text.gameObject.SetActive(true);
+        _recordedText.text = _currentString;
+        _recordedText.gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// Hides the text of the bubble.
+    /// </summary>
     public void HideTranscribedText()
     {
-        _text.gameObject.SetActive(false);
+        _recordedText.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Repeats the set AudioClip.
+    /// </summary>
     public void RepeatAudioClip()
     {
         if(_audioClip == null) return;
-
-        // this was for test to convert audio to string and retrieve it from playerprefs
-        // var stringAudio = SpeechToTextManager.Instance.AudioClipToString(_audioClip);
-        // PlayerPrefs.SetString("recording", stringAudio);
-        // _audioSource.clip = SpeechToTextManager.Instance.StringToAudioClip(PlayerPrefs.GetString("recording"), _audioClip.name, _audioClip.channels, _audioClip.frequency);
 
         _audioSource.clip = _audioClip;
         _audioSource.Play();
     }
 
+    /// <summary>
+    /// Deletes bubbles Data.
+    /// </summary>
     public void DeleteData()
     {
         _audioClip = null;
         _currentString = "";
+        _recordedText.text = "";
+        _percentageSimilarity.text = "";
     }
 
+    /// <summary>
+    /// Returns whether the bubble has data. True if it has false otherwise.
+    /// </summary>
     public bool HasData()
     {
         return (_audioClip != null);
     }
+    
+    #endregion
+
+    #region Public Getters of Bubble Data
+
+    /// <summary>
+    /// Get the current text of the bubble.
+    /// </summary>
+    public string GetBubbleText()
+    {
+        return _currentString;
+    }
+
+    /// <summary>
+    /// Returns the bubbles audio clip as a string.
+    /// </summary>
+    public string GetBubbleAudioClipAsString()
+    {
+        return SpeechToTextManager.Instance.AudioClipToString(_audioClip);
+    }
+
+    #endregion
+
+    #region Public Setters of the Bubble Data
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SetBubbleText(string text)
+    {
+        _currentString = text;
+    }
+
+    /// <summary>
+    /// Sets the bubbles audio clip from string.
+    /// </summary>
+    public void SetBubbleAudioClipFromString(string audioClipString)
+    {
+        _audioClip = SpeechToTextManager.Instance.StringToAudioClip(audioClipString, _audioClip.name, _audioClip.channels, _audioClip.frequency);
+    }
+
+    #endregion
 }
