d709375795293c3de62325d9ca2b832e489059bd
Mika <mikameel@outlook.com>
2024-06-10 09:35:31 +0200

    BubbleData prefab

---
+++ Assets/BubbleData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleData : MonoBehaviour
+{
+    public int slotId;
+    public string objectString;
+    public string text;
+    public string audio;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
